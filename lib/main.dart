16bbeaf44ca65512c1b26ba33c76d1ba5400bbc9
Devair Luiz Moitim <dmoitim@gmail.com>
2020-03-24 00:47:25 -0300

    Criação da Home iOS do aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:imc_flutter/ui/android/material-app.dart';
 import 'package:imc_flutter/ui/ios/cupertino-app.dart';
 
-void main() => runApp(MyMaterialApp());
-//void main() => runApp(MyCupertinoApp());
+//void main() => runApp(MyMaterialApp());
+void main() => runApp(MyCupertinoApp());
